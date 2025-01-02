d1e7389004ea5708940c45842a0be643ea9050c6
nico <npuhlmann@mailbox.org>
2025-01-02 15:53:46 +0100

    added flutter launcher icon

---
+++ lib/domain/models/profile.freezed.dart
@@ -0,0 +1,264 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'profile.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Profile _$ProfileFromJson(Map<String, dynamic> json) {
+  return _Profile.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Profile {
+  String get first_name => throw _privateConstructorUsedError;
+  set first_name(String value) =>
+      throw _privateConstructorUsedError; // Standardwert: leerer String
+  String get last_name =>
+      throw _privateConstructorUsedError; // Standardwert: leerer String
+  set last_name(String value) =>
+      throw _privateConstructorUsedError; // Standardwert: leerer String
+  DateTime? get date_of_birth =>
+      throw _privateConstructorUsedError; // Standardwert: leerer String
+  set date_of_birth(DateTime? value) => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
+  set email(String value) =>
+      throw _privateConstructorUsedError; // Standardwert: leerer String
+  String get imageUrl =>
+      throw _privateConstructorUsedError; // Standardwert: leerer String
+  set imageUrl(String value) => throw _privateConstructorUsedError;
+
+  /// Serializes this Profile to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of Profile
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ProfileCopyWith<Profile> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProfileCopyWith<$Res> {
+  factory $ProfileCopyWith(Profile value, $Res Function(Profile) then) =
+      _$ProfileCopyWithImpl<$Res, Profile>;
+  @useResult
+  $Res call(
+      {String first_name,
+      String last_name,
+      DateTime? date_of_birth,
+      String email,
+      String imageUrl});
+}
+
+/// @nodoc
+class _$ProfileCopyWithImpl<$Res, $Val extends Profile>
+    implements $ProfileCopyWith<$Res> {
+  _$ProfileCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Profile
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? first_name = null,
+    Object? last_name = null,
+    Object? date_of_birth = freezed,
+    Object? email = null,
+    Object? imageUrl = null,
+  }) {
+    return _then(_value.copyWith(
+      first_name: null == first_name
+          ? _value.first_name
+          : first_name // ignore: cast_nullable_to_non_nullable
+              as String,
+      last_name: null == last_name
+          ? _value.last_name
+          : last_name // ignore: cast_nullable_to_non_nullable
+              as String,
+      date_of_birth: freezed == date_of_birth
+          ? _value.date_of_birth
+          : date_of_birth // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ProfileImplCopyWith<$Res> implements $ProfileCopyWith<$Res> {
+  factory _$$ProfileImplCopyWith(
+          _$ProfileImpl value, $Res Function(_$ProfileImpl) then) =
+      __$$ProfileImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String first_name,
+      String last_name,
+      DateTime? date_of_birth,
+      String email,
+      String imageUrl});
+}
+
+/// @nodoc
+class __$$ProfileImplCopyWithImpl<$Res>
+    extends _$ProfileCopyWithImpl<$Res, _$ProfileImpl>
+    implements _$$ProfileImplCopyWith<$Res> {
+  __$$ProfileImplCopyWithImpl(
+      _$ProfileImpl _value, $Res Function(_$ProfileImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Profile
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? first_name = null,
+    Object? last_name = null,
+    Object? date_of_birth = freezed,
+    Object? email = null,
+    Object? imageUrl = null,
+  }) {
+    return _then(_$ProfileImpl(
+      first_name: null == first_name
+          ? _value.first_name
+          : first_name // ignore: cast_nullable_to_non_nullable
+              as String,
+      last_name: null == last_name
+          ? _value.last_name
+          : last_name // ignore: cast_nullable_to_non_nullable
+              as String,
+      date_of_birth: freezed == date_of_birth
+          ? _value.date_of_birth
+          : date_of_birth // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ProfileImpl with DiagnosticableTreeMixin implements _Profile {
+  _$ProfileImpl(
+      {this.first_name = '',
+      this.last_name = '',
+      this.date_of_birth = null,
+      this.email = '',
+      this.imageUrl = ''});
+
+  factory _$ProfileImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ProfileImplFromJson(json);
+
+  @override
+  @JsonKey()
+  String first_name;
+// Standardwert: leerer String
+  @override
+  @JsonKey()
+  String last_name;
+// Standardwert: leerer String
+  @override
+  @JsonKey()
+  DateTime? date_of_birth;
+  @override
+  @JsonKey()
+  String email;
+// Standardwert: leerer String
+  @override
+  @JsonKey()
+  String imageUrl;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Profile(first_name: $first_name, last_name: $last_name, date_of_birth: $date_of_birth, email: $email, imageUrl: $imageUrl)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Profile'))
+      ..add(DiagnosticsProperty('first_name', first_name))
+      ..add(DiagnosticsProperty('last_name', last_name))
+      ..add(DiagnosticsProperty('date_of_birth', date_of_birth))
+      ..add(DiagnosticsProperty('email', email))
+      ..add(DiagnosticsProperty('imageUrl', imageUrl));
+  }
+
+  /// Create a copy of Profile
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ProfileImplCopyWith<_$ProfileImpl> get copyWith =>
+      __$$ProfileImplCopyWithImpl<_$ProfileImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ProfileImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Profile implements Profile {
+  factory _Profile(
+      {String first_name,
+      String last_name,
+      DateTime? date_of_birth,
+      String email,
+      String imageUrl}) = _$ProfileImpl;
+
+  factory _Profile.fromJson(Map<String, dynamic> json) = _$ProfileImpl.fromJson;
+
+  @override
+  String get first_name;
+  set first_name(String value); // Standardwert: leerer String
+  @override
+  String get last_name; // Standardwert: leerer String
+  set last_name(String value); // Standardwert: leerer String
+  @override
+  DateTime? get date_of_birth; // Standardwert: leerer String
+  set date_of_birth(DateTime? value);
+  @override
+  String get email;
+  set email(String value); // Standardwert: leerer String
+  @override
+  String get imageUrl; // Standardwert: leerer String
+  set imageUrl(String value);
+
+  /// Create a copy of Profile
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ProfileImplCopyWith<_$ProfileImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/domain/models/profile.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ProfileImpl _$$ProfileImplFromJson(Map<String, dynamic> json) =>
+    _$ProfileImpl(
+      first_name: json['first_name'] as String? ?? '',
+      last_name: json['last_name'] as String? ?? '',
+      date_of_birth: json['date_of_birth'] == null
+          ? null
+          : DateTime.parse(json['date_of_birth'] as String),
+      email: json['email'] as String? ?? '',
+      imageUrl: json['imageUrl'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$$ProfileImplToJson(_$ProfileImpl instance) =>
+    <String, dynamic>{
+      'first_name': instance.first_name,
+      'last_name': instance.last_name,
+      'date_of_birth': instance.date_of_birth?.toIso8601String(),
+      'email': instance.email,
+      'imageUrl': instance.imageUrl,
+    };
